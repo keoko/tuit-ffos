4430d1e8cf2870b4bfb1d0f2f8a888c58bd13e19
Ignacio Cabre Barrera <cabrei@gsicommece.com>
2014-06-22 18:12:59 +0200

    set clj/cljs directory structure and added twitter sign-in image

--- hello.js
+++
@@ -1,6 +0,0 @@
-var button = document.getElementById('myButton');
-var txtInput = document.getElementById('myTextInput');
-button.addEventListener('click', function() {
-    var text = txtInput.value;
-    alert(text);
-})
